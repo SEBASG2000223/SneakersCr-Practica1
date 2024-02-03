142365e0c9e949e619c527d80d8c0af6d0b5fa41
sebas <sebas@DESKTOP-NEMJAN6>
2024-02-03 14:31:06 -0600

    Practica 1 MN

---
+++ src/test/java/com/SneakersCr/SneakersCrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SneakersCr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersCrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
